2434485af0adca5c443a1f68ddfeada2d4d23bfb
Evgeniy Shevtsov <zhenya_861533@tut.by>
2018-10-14 20:40:03 +0300

    Replace app module with splash module

--- app/src/main/java/by/mogyjib/guitarnotes/GuitarNotesApplication.kt
+++ app/src/main/java/by/mogyjib/guitarnotes/GuitarNotesApplication.kt
@@ -1,13 +1,14 @@
 package by.mogyjib.guitarnotes
 
 import android.app.Application
-import by.mogyjib.guitarnotes.main.dependency.appModule
+import by.mogyjib.guitarnotes.main.splash.splashModule
 import org.koin.android.ext.android.startKoin
+
 
 class GuitarNotesApplication : Application(){
     override fun onCreate() {
         super.onCreate()
 
-        startKoin(this, listOf(appModule))
+        startKoin(this, listOf(splashModule))
     }
 }
--- app/src/main/java/by/mogyjib/guitarnotes/main/dependency/AppModule.kt
+++
@@ -1,7 +0,0 @@
-package by.mogyjib.guitarnotes.main.dependency
-
-import org.koin.dsl.module.module
-
-val appModule = module {
-
-}
