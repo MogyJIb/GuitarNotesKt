5b5c8d7883007c327ffa9e56a10950736ce48e4f
Evgeniy Shevtsov <zhenya_861533@tut.by>
2018-10-20 17:07:35 +0300

    Refactor songlist with bottom app bar

--- app/src/main/java/by/mogyjib/guitarnotes/main/songs/view/SongListFragment.kt
+++ app/src/main/java/by/mogyjib/guitarnotes/main/songs/view/SongListFragment.kt
@@ -5,18 +5,21 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import by.mogyjib.guitarnotes.R
-import by.mogyjib.guitarnotes.base.view.BaseContract
 import by.mogyjib.guitarnotes.base.view.BaseFragment
+import kotlinx.android.synthetic.main.fragment_songlist.*
 
 
 class SongListFragment : BaseFragment(), SongListContract.View {
 
-    override val presenter: BaseContract.Presenter
-        get() = TODO("not implemented") //To change initializer of created properties use File | Settings | File Templates.
+    override val presenter = SongListPresenter()
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
-                              savedInstanceState: Bundle?): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_songlist, container, false)
+                              savedInstanceState: Bundle?): View?
+            = inflater.inflate(R.layout.fragment_songlist, container, false)
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        bottom_app_bar.inflateMenu(R.menu.songlist_bottom_menu)
     }
 }
--- app/src/main/java/by/mogyjib/guitarnotes/main/songs/view/SongListPresenter.kt
+++ app/src/main/java/by/mogyjib/guitarnotes/main/songs/view/SongListPresenter.kt
@@ -2,7 +2,7 @@ package by.mogyjib.guitarnotes.main.songs.view
 
 import by.mogyjib.guitarnotes.base.view.BasePresenter
 
-class SongListPresenter : BasePresenter<SongListContract.View>() {
+class SongListPresenter : BasePresenter<SongListContract.View>(), SongListContract.Presenter {
 
     override fun init() {
 
