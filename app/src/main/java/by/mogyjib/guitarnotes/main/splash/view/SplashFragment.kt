f86bcb9b7150eeccf5f2465357956d1645bb6ab9
Evgeniy Shevtsov <zhenya_861533@tut.by>
2018-10-20 17:07:55 +0300

    Refactor splash fragment code

--- app/src/main/java/by/mogyjib/guitarnotes/main/splash/view/SplashFragment.kt
+++ app/src/main/java/by/mogyjib/guitarnotes/main/splash/view/SplashFragment.kt
@@ -15,10 +15,9 @@ class SplashFragment : BaseFragment(), SplashContract.View {
     override val presenter: SplashPresenter by inject()
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
-                              savedInstanceState: Bundle?): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_splash, container, false)
-    }
+                              savedInstanceState: Bundle?): View?
+            = inflater.inflate(R.layout.fragment_splash, container, false)
+
 
     override fun openMainScreen() {
         router().navigate(R.id.action_splash_to_songlist)
