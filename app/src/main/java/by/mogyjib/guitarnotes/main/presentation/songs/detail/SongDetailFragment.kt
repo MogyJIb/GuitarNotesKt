75994f6052fdb4475048f18c4f18154e51ab1741
Evgeniy Shevtsov <eshevtsov@intervale.ru>
2018-12-05 10:59:27 +0300

    Disable song detail edits

--- app/src/main/java/by/mogyjib/guitarnotes/main/presentation/songs/detail/SongDetailFragment.kt
+++ app/src/main/java/by/mogyjib/guitarnotes/main/presentation/songs/detail/SongDetailFragment.kt
@@ -7,6 +7,8 @@ import android.view.ViewGroup
 import by.mogyjib.guitarnotes.R
 import by.mogyjib.guitarnotes.main.data.models.Song
 import by.mogyjib.guitarnotes.main.presentation.songs.BaseSongFragment
+import by.mogyjib.guitarnotes.utils.disable
+import by.mogyjib.guitarnotes.utils.enable
 import kotlinx.android.synthetic.main.fragment_song_detail.*
 import org.koin.android.ext.android.inject
 
@@ -32,5 +34,9 @@ class SongDetailFragment : BaseSongFragment(), SongDetailContract.View {
             author_edit_text.setText(author)
             text_edit_text.setText(text)
         }
+
+        name_input_layout.disable()
+        author_input_layout.disable()
+        text_input_layout.disable()
     }
 }
